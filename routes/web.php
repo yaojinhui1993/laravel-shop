2cf8a4240c339771e8d4c771be84f42fa3273d88
yaojinhui <yaojinhui1993@gmail.com>
2021-01-13 15:10:26 +0800

    shipping address

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'PagesController@root')->name('root');
 
 Auth::routes(['verify' => true]);
+
+Route::group(['middleware' => ['auth', 'verified']], function () {
+    Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+});
