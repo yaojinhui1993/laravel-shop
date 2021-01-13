e056b6419f1b7adbb13d68207987cf80b60a8f2f
yaojinhui <yaojinhui1993@gmail.com>
2021-01-13 13:57:11 +0800

    Register and login

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -19,13 +19,29 @@
         {{-- Right Side Of Navbar --}}
         <ul class="navbar-nav navbar-right">
             {{-- Authentication Links --}}
-            <li class="nav-item">
-                <a href="#" class="nav-link">登录</a>
-            </li>
+            @guest
+                <li class="nav-item">
+                    <a href="{{ route('login') }}" class="nav-link">登录</a>
+                </li>
 
-            <li class="nav-item">
-                <a href="#" class="nav-link">注册</a>
-            </li>
+                <li class="nav-item">
+                    <a href="{{ route('register') }}" class="nav-link">注册</a>
+                </li>
+            @else
+                <li class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <img src="https://cdn.learnku.com/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60" class="img-responsive img-circle" width="30px" height="30px">
+                        {{ Auth::user()->name }}
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a href="#" class="dropdown-item" id="logout" onclick="event.preventDefault();document.getElementById('logout-form').submit();">退出登录</a>
+                        <form action="{{ route('logout') }}" id="logout-form" method="POST" style="display:none;">
+                            {{ csrf_field() }}
+                        </form>
+                    </div>
+                </li>
+            @endguest
+            {{-- End Authentication Links --}}
         </ul>
     </div>
 </nav>
