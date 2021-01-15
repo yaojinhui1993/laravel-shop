b2fb4c5e46a888856933c068de9492aac3a0d53e
yaojinhui <yaojinhui1993@gmail.com>
2021-01-15 16:39:46 +0800

    order list

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -7,6 +7,7 @@ use App\Models\Order;
 use App\Jobs\CloseOrder;
 use App\Models\ProductSku;
 use App\Models\UserAddress;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Requests\OrderRequest;
 use App\Exceptions\InvalidRequestException;
@@ -75,5 +76,17 @@ class OrdersController extends Controller
         });
 
         return $order;
+    }
+
+    public function index(Request $request)
+    {
+        $orders = Order::query()
+            // 使用 with 方法预加载，避免 N+1 问题
+            ->with(['items.product', 'items.productSku'])
+            ->where('user_id', $request->user()->id)
+            ->orderBy('created_at', 'desc')
+            ->paginate();
+
+        return view('orders.index', ['orders' => $orders]);
     }
 }
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -28,6 +28,7 @@
                     <a href="{{ route('register') }}" class="nav-link">注册</a>
                 </li>
             @else
+
                 <li class="nav-item">
                     <a href="{{ route('cart.index') }}" class="nav-link mt-1">
                         <i class="fa fa-shopping-cart"></i>
@@ -41,6 +42,7 @@
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                         <a href="{{ route('user_addresses.index') }}" class="dropdown-item">收货地址</a>
+                        <a href="{{ route('orders.index') }}" class="dropdown-item">我的订单</a>
                         <a href="{{ route('products.favorites') }}" class="dropdown-item">我的收藏</a>
                         <a href="#" class="dropdown-item" id="logout" onclick="event.preventDefault();document.getElementById('logout-form').submit();">退出登录</a>
                         <form action="{{ route('logout') }}" id="logout-form" method="POST" style="display:none;">
