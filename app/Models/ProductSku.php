db8e286be54b50c9f4d86a492a30d1d47033e004
yaojinhui <yaojinhui1993@gmail.com>
2021-01-15 15:58:13 +0800

    create order

--- app/Models/ProductSku.php
+++ app/Models/ProductSku.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Exceptions\InternalException;
 use Illuminate\Database\Eloquent\Model;
 
 class ProductSku extends Model
@@ -13,5 +14,23 @@ class ProductSku extends Model
     public function product()
     {
         return $this->belongsTo(Product::class);
+    }
+
+    public function decreaseStock($amount)
+    {
+        if ($amount < 0) {
+            throw new InternalException('减库存不可小于 0');
+        }
+
+        return $this->where('id', $this->id)->where('stock', '>=', $amount)->decrement('stock', $amount);
+    }
+
+    public function addStock($amount)
+    {
+        if ($amount < 0) {
+            throw new InternalException('加库存不可小于 0');
+        }
+
+        $this->increment('stock', $amount);
     }
 }
