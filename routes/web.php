e546d4b05a1951693e919aedd4d83582f7eb5371
yaojinhui <yaojinhui1993@gmail.com>
2021-01-15 10:32:51 +0800

    favored products

--- routes/web.php
+++ routes/web.php
@@ -32,4 +32,7 @@ Route::group(['middleware' => ['auth', 'verified']], function () {
     Route::put('user_addresses/{userAddress}', 'UserAddressesController@update')->name('user_addresses.update');
 
     Route::delete('user_addresses/{userAddress}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
+
+    Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
+    Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
 });
