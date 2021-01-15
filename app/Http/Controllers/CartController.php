21871e524977b7c1cad57d494499b4ae633dc219
yaojinhui <yaojinhui1993@gmail.com>
2021-01-15 14:18:08 +0800

    cart page

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\CartItem;
+use App\Models\ProductSku;
+use Illuminate\Http\Request;
 use App\Http\Requests\AddCartRequest;
 
 class CartController extends Controller
@@ -26,6 +28,22 @@ class CartController extends Controller
             $cart->productSku()->associate($skuId);
             $cart->save();
         }
+
+        return [];
+    }
+
+    public function index(Request $request)
+    {
+        $cartItems = $request->user()->cartItems()->with(['productSku.product'])->get();
+
+        return view('cart.index', [
+            'cartItems' => $cartItems,
+        ]);
+    }
+
+    public function remove(ProductSku $sku, Request $request)
+    {
+        $request->user()->cartItems()->where('product_sku_id', $sku->id)->delete();
 
         return [];
     }
