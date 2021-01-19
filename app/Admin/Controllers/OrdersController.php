b56867cbea4d02b49e48525031c21eedc87a7e59
yaojinhui <yaojinhui1993@gmail.com>
2021-01-19 14:59:21 +0800

    refuse refund

--- app/Admin/Controllers/OrdersController.php
+++ app/Admin/Controllers/OrdersController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 use Encore\Admin\Layout\Content;
 use App\Exceptions\InvalidRequestException;
 use Encore\Admin\Controllers\AdminController;
+use App\Http\Requests\Admin\HandleRefundRequest;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 
 class OrdersController extends AdminController
@@ -101,5 +102,30 @@ class OrdersController extends AdminController
 
         // 返回上一页
         return redirect()->back();
+    }
+
+    public function handleRefund(Order $order, HandleRefundRequest $request)
+    {
+        // 判断订单状态是否正确
+        if ($order->refund_status !== Order::REFUND_STATUS_APPLIED) {
+            throw new InvalidRequestException('订单状态不正确');
+        }
+
+        // 是否同意退款
+        if ($request->input('agree')) {
+            // 同意退款的逻辑先留空
+            // todo
+        } else {
+            // 将拒绝退款理由放到订单的 extra 字段中
+            $extra = $order->extra ?: [];
+            $extra['refund_disagree_reason'] = $request->input('reason');
+            // 将订单的退款状态改为未退款
+            $order->update([
+                'refund_status' => Order::REFUND_STATUS_PENDING,
+                'extra' => $extra,
+            ]);
+        }
+
+        return $order;
     }
 }
