7932755be217120a8ddc141700e49f883fa3f946
yaojinhui <yaojinhui1993@gmail.com>
2021-01-25 09:48:48 +0800

    Initialize installment models

--- app/helpers.php
+++ app/helpers.php
@@ -1,10 +1,18 @@
 <?php
 
+use Moontoast\Math\BigNumber;
 use Illuminate\Support\Facades\Route;
 
 if (! function_exists('route_class')) {
     function route_class()
     {
         return str_replace('.', '-', Route::currentRouteName());
+    }
+}
+
+if (! function_exists('big_number')) {
+    function big_number($number, $scale = 2)
+    {
+        return new BigNumber($number, $scale);
     }
 }
---
+++ database/migrations/2021_01_25_011944_create_installments_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use App\Models\Installment;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInstallmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('installments', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('no')->unique();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->unsignedBigInteger('order_id');
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
+            $table->decimal('total_amount');
+            $table->unsignedInteger('count');
+            $table->float('fee_rate');
+            $table->float('fine_rate');
+            $table->string('status')->default(Installment::STATUS_PENDING);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('installments');
+    }
+}
