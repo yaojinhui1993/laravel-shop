25e83032030a0614d1192272bfba4e13c8bc55ed
yaojinhui <yaojinhui1993@gmail.com>
2021-01-25 11:37:18 +0800

    Alipay installments

--- resources/views/installments/show.blade.php
+++ resources/views/installments/show.blade.php
@@ -45,7 +45,7 @@
                                 </div>
 
                                 <div class="payment-buttons">
-                                    <a href="" class="btn btn-primary btn-sm">支付宝支付</a>
+                                    <a href="{{ route('installments.alipay', ['installment' => $installment->id]) }}" class="btn btn-primary btn-sm">支付宝支付</a>
                                     <button class="btn-btn-sm btn-success" id="btn-wechat">微信支付</button>
                                 </div>
                             @endif
