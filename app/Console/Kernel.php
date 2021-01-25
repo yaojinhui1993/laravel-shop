18b66e5cef5d6e130ba4a88ded5a170e22fd68b2
yaojinhui <yaojinhui1993@gmail.com>
2021-01-25 14:31:18 +0800

    calculcate installment fine

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,6 +27,8 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')->hourly();
         // 每分钟执行一次
         $schedule->command('cron:finish-crowdfunding')->everyMinute();
+
+        $schedule->command('cron:calculate-installment-fine')->daily();
     }
 
     /**
