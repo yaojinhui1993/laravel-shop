97eec72f6658293bd6cc8599c44e70b39f28a86d
yaojinhui <yaojinhui1993@gmail.com>
2021-01-26 09:24:24 +0800

    Product properties model

---
+++ app/Models/ProductProperty.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProductProperty extends Model
+{
+    protected $fillable = ['name', 'value'];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -122,7 +122,17 @@
                         </ul>
                         <div class="tab-content">
                             <div class="tab-pane active" role="tabpane" id="product-detail-tab">
-                                {!! $product->description !!}
+                                <div class="properties-list">
+                                    <div class="properties-list-title">产品参数：</div>
+                                    <ul class="properties-list-body">
+                                        @foreach ($product->grouped_properties as $name => $values)
+                                            <li>{{ $name }}: {{ join(' ', $values) }}</li>
+                                        @endforeach
+                                    </ul>
+                                </div>
+                                <div class="product-description">
+                                    {!! $product->description !!}
+                                </div>
                             </div>
                             <div class="tab-pane" id="product-reviews-tab" role="tabpane">
                                 <table class="table table-bordered table-striped">
